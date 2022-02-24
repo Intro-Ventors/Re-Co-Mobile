82d3dfb23a0e34dfd83de8f122091898bac6b15e
spaidz <[kgaveen@gmail.com]>
2022-02-24 14:23:46 +0530

    imolemented the qr code code scanner view

---
+++ reality_core/lib/screens/home/qr_code_scanner.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+class QRScanner extends StatefulWidget {
+  const QRScanner({Key? key}) : super(key: key);
+
+  @override
+  _QRScannerState createState() => _QRScannerState();
+}
+
+class _QRScannerState extends State<QRScanner> {
+  final qrKey = GlobalKey(debugLabel: 'QR');
+
+  QRViewController? controller;
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) => SafeArea(
+          child: Scaffold(
+        body: Stack(
+          alignment: Alignment.center,
+          children: <Widget>[
+            buildQRView(context),
+          ],
+        ),
+      ));
+
+  Widget buildQRView(BuildContext context) => QRView(
+        key: qrKey,
+        onQRViewCreated: onQRViewCreated,
+        overlay: QrScannerOverlayShape(),
+      );
+
+  void onQRViewCreated(QRViewController controller) {
+    setState(() {
+      this.controller = controller;
+    });
+  }
+}
