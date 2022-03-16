c4caf221cf528652039d2b8537521e939ca924cd
spaidz <kgaveen@gmail.com>
2022-03-16 23:03:10 +0530

    made model class for frequently asked questions and answeers

---
+++ reality_core/lib/screens/home/faq_page.dart
@@ -0,0 +1,62 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class FaqPage extends StatefulWidget {
+  const FaqPage({Key? key}) : super(key: key);
+
+  @override
+  State<FaqPage> createState() => _FaqPageState();
+}
+
+class _FaqPageState extends State<FaqPage> {
+  List<Object> _faqList = [];
+
+  @override
+  void didChangeDependancies() {
+    super.didChangeDependencies();
+    getUserQuestionList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("FAQ"),
+      ),
+      body: SafeArea(
+          child: ListView.builder(
+              itemCount: _faqList.length,
+              itemBuilder: (context, index) {
+                return Text("${index}");
+              })),
+    );
+  }
+
+  Future getUserQuestionList() async {
+    var data = await FirebaseFirestore.instance
+        .collection('faq_collection')
+        .doc()
+        .get();
+
+    /* setState(() {
+      _faqList = List.from(data.docs.map((doc) => Question.fromSnapshot(doc)));
+    }); */
+  }
+}
+
+
+
+
+/* class DetailPage extends StatefulWidget {
+  const DetailPage({Key? key}) : super(key: key);
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+} */
