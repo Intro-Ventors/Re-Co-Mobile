dde46e753af0fb0413715cfbdb04b4e6412e561c
Mark <markjulagting2003@gmail.com>
2022-04-08 19:18:02 +0530

    creating peer to peer conection

---
+++ .history/reality_core/lib/signaling_20220408182153.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
+
+typedef void StreamStateCallback(MediaStream stream);
+
+class Signaling {
+  Map<String, dynamic> configuration = {};
+
+  RTCPeerConnection? peerConnection;
+  MediaStream? localStream;
+  MediaStream? remoteStream;
+  String? roomId;
+  String? currentRoomText;
+  StreamStateCallback? onAddRemoteStream;
+}
