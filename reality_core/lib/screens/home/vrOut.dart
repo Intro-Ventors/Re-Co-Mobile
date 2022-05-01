cd895db1c8e3e6d19b604a97fa91bd03893b0ade
Messiah106 <rohithraahul@gmail.com>
2022-05-01 23:55:42 +0530

    Added screens

--- reality_core/lib/screens/home/vrOut.dart
+++ reality_core/lib/screens/home/vrOut.dart
@@ -1,8 +1,5 @@
 import 'package:flutter/material.dart';
-<<<<<<< HEAD
-
-=======
->>>>>>> ab5131d1889cddef28681c3da279768308ae9160
+import 'package:flutter_unity_widget/flutter_unity_widget.dart';
 
 class VROutScreen extends StatefulWidget {
   const VROutScreen({Key? key}) : super(key: key);
@@ -12,19 +9,96 @@ class VROutScreen extends StatefulWidget {
 }
 
 class _VROutScreenState extends State<VROutScreen> {
-<<<<<<< HEAD
-  
+  static final GlobalKey<ScaffoldState> _scaffoldKey =
+      GlobalKey<ScaffoldState>();
+  late UnityWidgetController _unityWidgetController;
+  double _sliderValue = 0.0;
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      
-      
-    );
-=======
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold();
->>>>>>> ab5131d1889cddef28681c3da279768308ae9160
+  void initState() {
+    super.initState();
   }
+
+
+@override 
+  Widget build(BuildContext context) {
+  return MaterialApp(
+      home: Scaffold(
+        key: _scaffoldKey,
+        appBar: AppBar(
+          title: const Text('Unity Flutter Demo'),
+        ),
+        body: Card(
+          margin: const EdgeInsets.all(8),
+          clipBehavior: Clip.antiAlias,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          child: Stack(
+            children: <Widget>[
+              UnityWidget(
+                  onUnityCreated: onUnityCreated,
+                  onUnityMessage: onUnityMessage,
+                  onUnitySceneLoaded: onUnitySceneLoaded,
+                  fullscreen: false,
+              ),
+              Positioned(
+                bottom: 20,
+                left: 20,
+                right: 20,
+                child: Card(
+                  elevation: 10,
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20),
+                        child: Text("Rotation speed:"),
+                      ),
+                      Slider(
+                        onChanged: (value) {
+                          setState(() {
+                            _sliderValue = value;
+                          });
+                          setRotationSpeed(value.toString());
+                        },
+                        value: _sliderValue,
+                        min: 0,
+                        max: 20,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  // Communcation from Flutter to Unity
+  void setRotationSpeed(String speed) {
+    _unityWidgetController.postMessage(
+      'Cube',
+      'SetRotationSpeed',
+      speed,
+    );
+  }
+
+  // Communication from Unity to Flutter
+  void onUnityMessage(message) {
+    print('Received message from unity: ${message.toString()}');
+  }
+
+  // Callback that connects the created controller to the unity controller
+  void onUnityCreated(controller) {
+    this._unityWidgetController = controller;
+  }
+
+  // Communication from Unity when new scene is loaded to Flutter
+  void onUnitySceneLoaded(SceneLoaded? sceneInfo) {
+    print('Received scene loaded from unity: ${sceneInfo?.name}');
+    print('Received scene loaded from unity buildIndex: ${sceneInfo?.buildIndex}');
+  }
+
 }
